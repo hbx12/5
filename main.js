0f0666d980fa462f74e745bbf9c50ae85ce69c8c
hbx12 <107771255+hbx12@users.noreply.github.com>
2022-06-19 03:02:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// GitHub @dakshcodes
+
+const chkbx = document.getElementById('chkbx');
+
+chkbx.addEventListener('change', () =>{
+    document.body.classList.toggle('dark');
+})
+
+// We're good to go
